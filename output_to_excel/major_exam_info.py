adf5a4ccfb4617d8f9e61dfe01cf550874ba4784
xx025 <71559822+xx025@users.noreply.github.com>
2021-04-14 21:06:47 +0800

    专硕

--- output_to_excel/major_exam_info.py
+++ output_to_excel/major_exam_info.py
@@ -8,14 +8,14 @@ with open('../uinnfo/link_loc_uni.json', encoding='utf8') as f:
 
 # 读文件夹文件，获取文件名列表
 u_list = []
-for filename in os.listdir("../college_majors_exam_scope/"):
+for filename in os.listdir("../college_majors_exam_scope/zyxw/"):
     u_list.append('{}'.format(filename[:-5]))
 
 # 读文件
 udict = {}
 allMagorsInfo = []
 for filename in u_list:
-    with open('../college_majors_exam_scope/{}.json'.format(filename), 'r', encoding='utf8') as f:
+    with open('../college_majors_exam_scope/zyxw/{}.json'.format(filename), 'r', encoding='utf8') as f:
         load_dict = json.load(f)
     udict[filename] = [u for u in load_dict[filename]]
     for k in load_dict[filename]:
@@ -33,4 +33,4 @@ sht.range('A1').value = '院校体招生专业、招生人数和考试科目等'
 sht.range('a1:k1').api.merge()  # 合并
 sht.range('A2:R2').value =list2
 sht.range('A3').options(expand='table').value = allMagorsInfo
-wb.save('../documentation/院校专业0812.xlsx')
+wb.save('../documentation/院校专业-专硕-0854.xlsx')
