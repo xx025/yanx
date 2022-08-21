74408c05ffeddb02bf78ded7830fef791944901d
xx025 <71559822+xx025@users.noreply.github.com>
2022-08-21 21:30:55 +0800

    Create get_major_details.py

---
+++ dl_s/get_major_details.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+from db import con
+
+r = con.execute('SELECT ksfw FROM recruit_major')
+
+url = 'https://yz.chsi.com.cn'
+
+for i in [i[-1] for i in r]:
+    res = requests.get(url + i)
+    soup = BeautifulSoup(res.text)
+
+    zsdw = soup.select('.zsml-condition tbody tr .zsml-summary')
+
+    zsde = soup.select('.zsml-result tbody tr td')
+    print([i.text for i in zsdw] + [i.text for i in zsde])
+
+    break
