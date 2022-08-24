90fd4b978144b4219156e076fda03284952e9136
xx025 <71559822+xx025@users.noreply.github.com>
2022-08-24 16:10:14 +0800

    Update get_major_details.py

--- dl_s/get_major_details.py
+++ dl_s/get_major_details.py
@@ -1,19 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
 
-from db import con
+from db import cur, con
+from deal_text import replace_bank
 
-r = con.execute('SELECT ksfw FROM recruit_major')
 
-url = 'https://yz.chsi.com.cn'
+class dl_details:
+    def __init__(self):
+        self.__data = []
+        self.__urls = []
 
-for i in [i[-1] for i in r]:
-    res = requests.get(url + i)
-    soup = BeautifulSoup(res.text)
+    def dl_data(self):
+        if len(self.__urls) == 0:
+            self.set_urls()
 
-    zsdw = soup.select('.zsml-condition tbody tr .zsml-summary')
+        count = len(self.__urls)
+        for l_j in range(len(self.__urls)):
+            k = self.__urls[l_j]
+            soup = BeautifulSoup(requests.get(k).text, features="html.parser")
 
-    zsde = soup.select('.zsml-result tbody tr td')
-    print([i.text for i in zsdw] + [i.text for i in zsde])
+            zsdw = soup.select('.zsml-condition tbody tr .zsml-summary')
+            zsdw = [replace_bank(i.text) for i in zsdw]
 
-    break
+            sid = k.split('=')[-1]
+            enrollment_unit = zsdw[0]
+            examination_method = zsdw[1]
+            departments = zsdw[2].split(')')[-1]
+            major = zsdw[3]
+            learning_style = zsdw[4]
+            research_direction = zsdw[5]
+            instructor = zsdw[6]
+            number_of_proposed_recruits = zsdw[7]
+
+            row1 = (sid, enrollment_unit, examination_method, departments, major, learning_style,
+                    research_direction, instructor, number_of_proposed_recruits)
+
+            cur.execute(
+                'insert into recruit_details (id, enrollment_unit, examination_method, '
+                'departments, major, learning_style, research_direction, instructor, number_recruit) '
+                'VALUES (?,?,?,?,?,?,?,?,?)', row1
+            )
+            scope_items = soup.select('.zsml-result .zsml-res-items')
+            data = []
+            for i in scope_items:
+                td = [replace_bank(k.text) for k in i.select('tr td')]
+                td.insert(0, sid)
+                data.append(td)
+            cur.executemany(
+                'INSERT INTO exam_scope (id, political, foreign_language, pro_course_1, pro_course_2) '
+                'VALUES (?,?,?,?,?)', data)
+            con.commit()
+
+            print(row1)
+            for j in data:
+                print(j)
+            print('{}/{}'.format(l_j, count))
+
+    def set_urls(self):
+        cons = cur.execute('select ksfw from recruit_major')
+        self.__urls = ['https://yz.chsi.com.cn/zsml/kskm.jsp?id=' + i[0] for i in cons]
