9b2a8c7d99513943f759cf002111b0d65acd1f18
xx025 <71559822+xx025@users.noreply.github.com>
2022-08-29 10:58:54 +0800

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -4,8 +4,8 @@ import sqlite3
 con = sqlite3.connect(os.getcwd() + '\db\database.db')
 cur = con.cursor()
 
-cur.execute('DELETE  from xuekemenlei_code')
-cur.execute('DELETE  from xuekelingyu_code')
+# cur.execute('DELETE  from xuekemenlei_code')
+# cur.execute('DELETE  from xuekelingyu_code')
 cur.execute('DELETE  from zhuanye_name')
 cur.execute('DELETE  from recruit_school')
 cur.execute('DELETE  from recruit_major')
