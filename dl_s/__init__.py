88b728f37d7db02098e139c23cf03768b18617ed
xx025 <71559822+xx025@users.noreply.github.com>
2022-08-29 22:32:07 +0800

    Update __init__.py

--- dl_s/__init__.py
+++ dl_s/__init__.py
@@ -3,9 +3,8 @@ from dl_s.get_recruit_majors import dl_majors
 from dl_s.get_recruit_schools import dl_schools
 
 
-@staticmethod
 class dlYzw:
     def __init__(self):
-        self.dl_majors = dl_majors()
         self.dl_schools = dl_schools()
+        self.dl_majors = dl_majors()
         self.dl_details = dl_details()
