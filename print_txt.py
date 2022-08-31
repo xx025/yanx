fb6a94db89a6083b0ea1d1d23dfbc1ce96e62983
xx025 <71559822+xx025@users.noreply.github.com>
2022-08-31 17:57:08 +0800

    Delete print_txt.py

--- print_txt.py
+++
@@ -1,9 +0,0 @@
-GLOBALS_DICT = {'text_area': None}
-
-
-def print_t(text):
-    try:
-        GLOBALS_DICT['text_area'].append(text)
-        GLOBALS_DICT['text_area'].moveCursor(GLOBALS_DICT['text_area'].textCursor().End)  # 文本框显示到底部
-    except Exception as e:
-        print(e)
