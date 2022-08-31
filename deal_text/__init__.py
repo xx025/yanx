18db286cf5ba380c530245145f803b0374b272be
xx025 <71559822+xx025@users.noreply.github.com>
2022-08-31 11:18:23 +0800

    Update __init__.py

--- deal_text/__init__.py
+++ deal_text/__init__.py
@@ -2,14 +2,3 @@ def replace_bank(strs=''):
     return strs.replace('\n', '').replace('\r', '').replace(' ', '')
 
 
-def replace_js_fun(js_fun_str=''):
-    js_fun_str = '''document.write(cutString('专业：4(不含推免)', 6));'''.split('\'')
-
-    return js_fun_str
-
-
-d = replace_js_fun()
-
-pass
-
-pass
