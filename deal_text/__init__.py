7e51db88426e4b593d442bc1eef327f60980ae5e
xx025 <71559822+xx025@users.noreply.github.com>
2022-09-02 00:28:11 +0800

    Update __init__.py

--- deal_text/__init__.py
+++ deal_text/__init__.py
@@ -1,2 +1,16 @@
 def replace_bank(strs=''):
     return strs.replace('\n', '').replace('\r', '').replace(' ', '')
+
+
+def get_url_param(url: str):
+    '''
+
+    :param url:
+    :return:
+    '''
+    ts = replace_bank(url)
+    param = {}
+    for i in ts.split('?')[-1].split('&'):
+        (key, val) = tuple(i.split('='))
+        param[key] = val
+    return param
