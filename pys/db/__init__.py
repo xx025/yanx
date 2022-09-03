1399e1f54286c7f76d5c0c675b754ceff8df57ad
xx025 <71559822+xx025@users.noreply.github.com>
2022-09-04 03:20:30 +0800

    x

--- pys/db/__init__.py
+++ pys/db/__init__.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
 
+from global_values import global_queue
+
 
 class db_con:
 
@@ -11,16 +13,13 @@ class db_con:
 
 
 def kks():
+    global_queue.put('正在初始化数据库')
     con = db_con.get_con()
     cur = con.cursor()
-    cur.execute('DELETE  from 专业代码')
     cur.execute('DELETE  from 招生院校索引')
     cur.execute('DELETE  from 招生专业索引')
     cur.execute('DELETE  from 考试范围')
     cur.execute('DELETE  from 招生专业')
     con.commit()
     con.close()
-
-
-kks()
-
+    global_queue.put('初始化数据库完成')
