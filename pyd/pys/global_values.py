0aca2403bf367c12f3fda0cf9b8b322797a589d8
xx025 <71559822+xx025@users.noreply.github.com>
2022-09-17 02:44:39 +0800

    Update global_values.py

--- pyd/pys/global_values.py
+++ pyd/pys/global_values.py
@@ -7,9 +7,11 @@ global_queue = Queue(maxsize=0)
 GLOBALS_DICT = {'text_area': None,
                 'down_end': False,
                 'out_path': None,
-                'file_name': ''}
+                'file_name': '',
+                'MAX_SCOPE': 0}
 
-upload_yanx_docs_tips = "你是否愿意将您下载的目录上传到 GitHub 文档库 以帮助更多的同学？ (这不会占用你太多时间)"
-yanx_docs_url = 'https://github.com/xx025/YanX-Docs#提交步骤'
+
+UPLOAD_TIPS = "亲爱的，你的目录下载完了！；你是否愿意将您下载的目录上传到 GitHub 仓库 以帮助更多的同学？ (这不会占用你太多时间，在你上传后，将被列入YanX-Docs 这个仓库贡献者名单)"
+YAN_X_DOCS_URL = 'https://github.com/xx025/YanX-Docs#提交步骤'
 MainTitle = 'YanX-研招网专业目录下载-{}'.format('2023')
-WindowTitle = MainTitle + '-v0.1.{}.{}'.format(9, 11)
+WindowTitle = MainTitle + '-v0.1.{}.{}'.format(9, 16)
