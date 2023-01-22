722b85f76b0bcd59dbfe678a979a953fa381ddf7
xx025 <71559822+xx025@users.noreply.github.com>
2023-01-22 10:46:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from PyQt5 import QtWidgets
 
-from db2 import Database
 from ui.static_def import out_date
 from ui.ui_action import Ui
 from verification import if_star
 
 if __name__ == "__main__":
     import sys
-
 
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
