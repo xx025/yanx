a25942d4381e583b3a3e7fdc9ea6fdbdb265e0c7
xx025 <71559822+xx025@users.noreply.github.com>
2023-01-22 10:46:47 +0800

    Update clst.py

--- task/clst.py
+++ task/clst.py
@@ -1,7 +1,3 @@
-from _g import GLOBAL_VAL
-from db2 import db_con
-
 TABLE_HEAD = ['学校', '所在地', 'AB区', '院校建设计划', '考试方式', '学院', '专业', '学习方式',
               '招生人数', '政治考试', '外语考试',
               '专业课1考试', '专业课二考试']
-
