e31c472b31658b2e09532d5c306633d82fe41ad9
a <a@a.a>
2023-08-04 18:41:37 +0800

    使用 uvicorn 启动

--- yweb/tools.py
+++ yweb/tools.py
@@ -1,7 +1,4 @@
-import os
 import time
-
-from yzw_dl.tools import output_jsonfile
 
 
 def create_file_name(dlparams, session):
@@ -14,24 +11,8 @@ def create_file_name(dlparams, session):
     # 将这些名字连接起来，但最多出现一个 - 作为分隔符
     name_list = [name_mllb, name_xklb, name_xxfs, name_yxdq, name_yxjh, name_zymc]
     name_list = [i for i in name_list if i != '']
-    # 加个 20230515-12:00 这样的时间戳
-    name_list.append(time.strftime("%Y%m%d-%H%M", time.localtime()))
+    # 加个 20230515 这样的时间戳
+    name_list.append(time.strftime("%Y%m%d", time.localtime()))
     return '-'.join(name_list)
 
 
-def save_jsonfile(data, save_name):
-    # 保存数据
-    out_file_path = os.path.join('static/dldocs', f'{save_name}.json')
-    output_jsonfile(data, out_file_path)
-
-
-def dlparams_to_id(dlparams, session):
-    """
-    将 dlparams 转换为 id
-    """
-    # 将字典的可以连接起来转换为字符串
-    id = ''
-    session['xklb']
-    session['mldm']
-
-    return '-'.join([str(i) for i in dlparams.values()])
