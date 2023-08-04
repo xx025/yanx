7bfe8db3a7258e2d255991e0c6cbfa6c1d3f8f7b
a <a@a.a>
2023-08-04 22:00:09 +0800

    .

--- utils/path.py
+++
@@ -1,7 +0,0 @@
-import winreg
-
-
-def get_desktop_path():
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
-    path = winreg.QueryValueEx(key, "Desktop")[0]
-    return path
